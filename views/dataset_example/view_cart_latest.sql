c423bd9c8a05cddce61c5316456add4f4b0becf2
Ivaylo Dimitrov <idimitrov.966@gmail.com>
2022-06-29 12:17:33 +0000

    project fixed

--- views/dataset_example/view_cart_latest.sql
+++ views/dataset_example/view_cart_latest.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE VIEW `<project_id>.dataset_example.view_cart_latest`
+CREATE OR REPLACE VIEW `<project_id>.id_test_build.view_cart_latest`
 OPTIONS(
 description="the latest representation of each cart"
 )
@@ -9,9 +9,9 @@ SELECT * EXCEPT(row_number)
      *,
      ROW_NUMBER() OVER(PARTITION BY Id ORDER BY PartitionTimestamp DESC) row_number
    FROM(
-    SELECT * FROM `<project_id>.dataset_example.cart_timeseries`
+    SELECT * FROM `<project_id>.id_test_build.cart_timeseries`
       UNION ALL
-    SELECT * FROM `<project_id>.dataset_example.cart_staging` WHERE _PARTITIONDATE >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY)
+    SELECT * FROM `<project_id>.id_test_build.cart_staging` WHERE _PARTITIONDATE >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 DAY)
    )
   ) 
   WHERE row_number = 1
